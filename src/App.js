093a3ac912d2036b497e58082045bfeebc33dbea
Хоменко Олена Володимирівна <Khomenko@khnpp.ua>
2023-03-24 11:47:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import styled from 'styled-components'
 
+
+const Header = styled.h2`
+  font-size: 1.5em;
+`
+// Створення Styled Component
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
+export const Button = styled.button`
+    display: block;
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: palevioletred;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+`;
+
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2)
+
+`;
+
+// прилкад створення компонента у вигляді класу
+// class Field extends Component {
+//   render() {
+//     const holder = 'Type here'
+//     const styledField = {
+//       width: '300px'
+//     }
+//     return (
+//       <input
+//         style={styledField}
+//         placeholder={holder}
+//         type="text" />
+//     )
+//   }
+// }
+// приклад створення функціонального компонента
+// const Field = () => {
+//   const holder = 'Type here'
+//   const styledField = {
+//     width: '300px'
+//   }
+//   return (
+//     <input
+//       style={styledField}
+//       placeholder={holder}
+//       type="text" />
+//   )
+// }
+
+// function Btn() {
+//   const text = 'Log in'
+//   const logged = false
+//   const res = () => {
+//     return text
+//   }
+//   return (
+//     <button>{logged ? 'Enter' : res()}</button>
+//   )
+// }
+// Створення  функціонального компонента с props
+// в якості пропсів можна передавати будь що
+// у прикладі name - просто строка
+// surname - обєкт
+// link - як функція
+// function WhoIAm({ name, surname, link }) {
+//   return (
+//     <div>
+//       <h2>'My name is {name}, surname - {surname.secondName}'</h2>
+//       <a href={link()}>My profile</a>
+//     </div>
+//   )
+// }
+
+
+// Створення классового компонента с props
+
+class WhoIAmClass extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      age: 50,
+      position: ''
+    }
+  }
+  // без отслеживания состояния
+  // nextYear = () => {
+  //   this.setState({
+  //     age: this.state.age + 1
+  //   })
+  // }
+  // с отслеживанием состояния
+  nextYear = () => {
+    this.setState(state => (
+      {
+        age: state.age + 1
+      }
+    ))
+  }
+
+  onCommitInput = (e) => {
+    this.setState({
+      position: e.target.value
+    })
+  }
+
+  render() {
+    const { name, surname, link } = this.props;
+    const { age, position } = this.state;
+    return (
+      <Wrapper>
+        <EmpItem>
+          <h2>Реалізація на класах</h2>
+          <Button onClick={this.nextYear}>+++</Button>
+          <h2>My name is {name},
+            surname - {surname},
+            my age - {age},
+            my position -  {position}
+          </h2>
+          <a href={link}>My profile</a>
+          <form>
+            <span>Введить свою посаду</span>
+            <input type="text" onChange={(e) => this.onCommitInput(e, 'Хованки')} />
+          </form>
+        </EmpItem>
+      </Wrapper>
+    )
+  }
+}
+
+
+// в якості пропсів можна передавати будь що
+// у прикладі name - просто строка
+// surname - обєкт
+// link - як функція
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Field />
+      <Btn />
+      <WhoIAm name='Olena' surname={{ secondName: 'Beatu' }} link={() => { return 'google.com' }} />
+      <WhoIAm name='Danya' surname={{ secondName: 'Strong' }} link={() => { return 'google.com' }} /> */}
+      <WhoIAmClass name='Olena' surname='Class' link='https://ru.reactjs.org/docs/state-and-lifecycle.html' />
+      <WhoIAmClass name='Victor' surname='Classnenko' link='https://ru.reactjs.org/docs/state-and-lifecycle.html' />
+      {/* <div className='buttonGr'><Button>Styled Component</Button></div> */}
     </div>
   );
 }
 
+
+export { Header };
 export default App;
---
+++ src/bootstrapTest.js
@@ -0,0 +1,82 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+const BootstrapTest = () => {
+    return (
+        <Container className='mt-5 mb-5'>
+            <Row>
+                <Col>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+                <Col>
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://static.espreso.tv/uploads/article/150653/images/im-%D0%B1%D0%B0%D1%88%D0%BD%D1%8F_7themes-su.jpg"
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://travel.24tv.ua/resources/photos/news/202010/1434043.jpg?v=1661267619000&w=840&h=472&fit=cover&output=webp&q=50"
+                                alt="Second slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Second slide label</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://www.accordtour.com/UserFiles/tour_city_gallery/fr/1104_Eyfeleva_bashnya/495x/01_eiffel_tower_07.webp"
+                                alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>
+                                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                                </p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </Col>
+            </Row>
+        </Container>
+    )
+
+}
+
+export default BootstrapTest;
